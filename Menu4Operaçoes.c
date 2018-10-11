d188475925520dad458b5a9130a9a42c0f8b3b22
luuised <42014080+luuised@users.noreply.github.com>
2018-10-11 19:21:50 -0300

    Recebe dois números e mostra 4 opções ao usuário

---
+++ Menu4Operaçoes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(){
+
+char resposta;
+float n1, n2; 
+resposta='0';
+
+
+
+//coleta de informações
+printf("Digite o primeiro numero..:");
+scanf("%f",&n1);
+printf("Digite o segundo numero..:");
+scanf("%f",&n2);
+
+//menu interativo
+
+printf("Escolha uma operacao\n");
+printf("1 - Media entre os numeros\n");
+printf("2 - Diferenca do maior pelo menor\n");
+printf("3 - Produto entre os numeros\n");
+printf("4 - Divisão do primeiro pelo segundo\n");
+fflush(stdin);
+scanf("%s",&resposta);
+fflush(stdin);
+ 
+//condições
+
+        if (resposta=='1') {
+            printf("A media entre os dois numeros é %.1f\n",((n1+n2)/2));}
+        else
+            if (resposta=='2') {
+                if (n1>n2) {
+                    printf("A diferença entre os dois numeros e %.1f\n",(n1-n2));}
+                else
+                    printf("A diferença entre os dois numeros e %.1f\n",(n2-n1));
+                }
+            else
+                if (resposta=='3') {
+                    printf("O produto dos dois numeros e %.1f\n",(n1*n2));}
+                else
+                    if(resposta=='4') {
+                        printf("A divisao do primeiro pelo segundo numero e %.1f\n",(n1/n2));}
+
+
+}
