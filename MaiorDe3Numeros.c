4e9b710927e85178bba63e767e8b263df8e2856c
luuised <42014080+luuised@users.noreply.github.com>
2018-10-11 17:18:02 -0300

    Add files via upload
    Recebe três números e mostra qual o maior deles

---
+++ MaiorDe3Numeros.c
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+int main() {
+
+int n1,n2,n3;
+
+printf("Digite o primeiro numero:...");
+scanf("%d",&n1);
+printf("Digite o segundo numero:...");
+scanf("%d",&n2);
+printf("Digite o terceiro:...");
+scanf("%d",&n3);
+
+
+        if (n1>n2&&n1>n3){
+        printf("O numero maior e %d\n",n1);}
+        else
+        if (n2>n3&&n2>n1){
+        printf("O numero maior e %d\n",n2);}
+        else 
+        if (n3>n2&&n3>n1){
+        printf("O numero maior e %d\n",n3);}
+
+
+
+
+
+
+}
